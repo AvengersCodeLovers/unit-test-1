261458f7e96235e7cdf8edda3c91109459221d29
hoangnx-2828 <nguyen.xuan.hoang@sun-asterisk.com>
2022-07-11 14:50:42 +0700

    Fix entity ticketInfo

--- src/Entity/TicketInfo.php
+++ src/Entity/TicketInfo.php
@@ -2,13 +2,15 @@
 
 namespace StdGroup\App\Entity;
 
+use DateTime;
+
 class TicketInfo
 {
-    public int $bookingDate;
+    public DateTime $bookingDate;
     public int $gender;
     public int $age;
 
-    public function __construct(\DateTime $bookingDate, int $gender, int $age)
+    public function __construct(DateTime $bookingDate, int $gender, int $age)
     {
         $this->bookingDate = $bookingDate;
         $this->gender = $gender;
